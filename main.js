9f71ccf9d9f9704d933425079ed7ae408f2fdf63
tank <35127914+tank0001@users.noreply.github.com>
2021-03-06 13:37:43 -0500

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ client.on("message", async message => {
   // ATTENTION !!!!!!! THIS IS WHERE YOU CAN CHANGE THE STATUS LOOK RIGHT BELOW
   // you can change the online thingy to 'online', 'idle', or 'dnd'
   // you can also change 'WATCHING' to 'PLAYING' or 'LISTENING'
-  client.user.setPresence({ activity: { name: 'excuse me maam not to be disrespectful or rude but could you please take this post down. that is my sister who was killed by a metra train. and it this post is very disrespectful. idk who u are or if u even know her but i need you to take this down please.', type: 'PLAYING'}, status: 'idle' })
+  client.user.setPresence({ activity: { name: 'excuse me maam not to be disrespectful or rude but could you please take this post down. that is m', type: 'PLAYING'}, status: 'idle' })
   
  if(message.author.bot) return;
   
